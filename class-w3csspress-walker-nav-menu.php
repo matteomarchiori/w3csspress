681cb05cd1cc2dc1c8c428590fe05b145367567a
Matteo Marchiori <matteo.marchiori97@gmail.com>
2022-03-11 19:35:16 +0100

    Added start_lvl docs

--- class-w3csspress-walker-nav-menu.php
+++ class-w3csspress-walker-nav-menu.php
@@ -24,6 +24,20 @@ if ( ! class_exists( 'w3csspress\W3csspress_Walker_Nav_Menu' ) ) {
 	class W3csspress_Walker_Nav_Menu extends \Walker_Nav_Menu {
 
 
+		/**
+		 * Starts the list before the elements are added.
+		 *
+		 * Starts the list before the elements are added.
+		 *
+		 * @since 2022.0
+		 *
+		 * @see Walker_Nav_Menu::start_lvl
+		 * @link https://developer.wordpress.org/reference/classes/walker_nav_menu/start_lvl/
+		 *
+		 * @param string $output Required. Used to append additional content (passed by reference).
+		 * @param int $depth Optional. Depth of menu item. Used for padding.
+		 * @param stdClass $args Optional. An object of wp_nav_menu() arguments. Default value: null.
+		 */
 		public function start_lvl( &$output, $depth = 0, $args = null ) {
 			$output .= '<ul class="w3-dropdown-content w3-animate-opacity w3-bar-block w3-mobile w3-theme-action sub-menu">';
 		}
