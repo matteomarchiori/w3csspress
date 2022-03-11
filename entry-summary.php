470cec8d7c40726c16ca247f5a4d036099f2b7ca
Matteo Marchiori <matteo.marchiori97@gmail.com>
2022-03-11 19:49:32 +0100

    Added entry summary docs

--- entry-summary.php
+++ entry-summary.php
@@ -1,3 +1,17 @@
+<?php
+/**
+ * Template for post summary
+ *
+ * This file is used to show summary of posts in WordPress.
+ *
+ * @package w3csspress
+ * @since 2022.0
+ */
+
+namespace w3csspress;
+
+?>
+
 <div class="entry-summary">
 	<?php if ( ( has_post_thumbnail() ) && ( ! is_search() ) ) : ?>
 		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?></a>
