4acc46675d1d3bc13cb1dcb2f41ed0d03aa272ec
Matteo Marchiori <matteo.marchiori97@gmail.com>
2022-03-11 19:30:01 +0100

    Added custom menu walker docs

--- class-w3csspress-walker-nav-menu.php
+++ class-w3csspress-walker-nav-menu.php
@@ -1,10 +1,28 @@
 <?php
 
+/**
+ * Class for custom menu walker
+ *
+ * This file is used to define a custom menu walker, with custom css classes.
+ *
+ * @link https://developer.wordpress.org/reference/classes/walker_nav_menu/
+ *
+ * @package w3csspress
+ * @since 2022.0
+ */
+
 namespace w3csspress;
 
 if ( ! class_exists( 'w3csspress\W3csspress_Walker_Nav_Menu' ) ) {
-
+	/**
+	 * Class for custom menu walker.
+	 *
+	 * This class is used to define a custom menu walker, with custom css classes.
+	 *
+	 * @since 2022.0
+	 */
 	class W3csspress_Walker_Nav_Menu extends \Walker_Nav_Menu {
+
 
 		public function start_lvl( &$output, $depth = 0, $args = null ) {
 			$output .= '<ul class="w3-dropdown-content w3-animate-opacity w3-bar-block w3-mobile w3-theme-action sub-menu">';
