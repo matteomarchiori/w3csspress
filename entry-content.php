7f1244281ffecca6cc6af2af7ed4b19586cec086
Matteo Marchiori <matteo.marchiori97@gmail.com>
2022-03-17 11:43:21 +0100

    Fixed bug url image

--- entry-content.php
+++ entry-content.php
@@ -14,7 +14,7 @@ namespace w3csspress;
 
 <div class="entry-content">
 	<?php if ( has_post_thumbnail() ) : ?>
-		<a href="<?php echo esc_url( wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full', false ) ); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'full' ); ?></a>
+		<a href="<?php echo esc_url( wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full', false )[0] ); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'full' ); ?></a>
 	<?php endif; ?>
 	<meta content="<?php echo esc_html( wp_strip_all_tags( get_the_excerpt(), true ) ); ?>" />
 	<?php the_content(); ?>
