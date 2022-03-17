f69b6978ee3b1a4e8fe3f4accd3caa6fcaf06f38
Matteo Marchiori <matteo.marchiori97@gmail.com>
2022-03-17 11:54:04 +0100

    Added check for menu existance

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template for header in WordPress
  *
@@ -12,14 +13,14 @@ namespace w3csspress;
 
 use w3csspress\W3csspress_Walker_Nav_Menu;
 
-get_template_part( 'class-w3csspress-walker-nav-menu' );
+get_template_part('class-w3csspress-walker-nav-menu');
 
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width" />
 	<?php wp_head(); ?>
 </head>
@@ -30,34 +31,36 @@ get_template_part( 'class-w3csspress-walker-nav-menu' );
 		<header id="header" role="banner">
 			<div id="branding">
 				<?php
-				if ( has_custom_logo() ) {
-					if ( function_exists( 'the_custom_logo' ) ) {
+				if (has_custom_logo()) {
+					if (function_exists('the_custom_logo')) {
 						the_custom_logo();
 					}
 				}
 				?>
 				<div id="site-title">
 					<?php
-					if ( is_front_page() || is_home() || is_front_page() && is_home() ) {
+					if (is_front_page() || is_home() || is_front_page() && is_home()) {
 						echo '<h1>';
 					}
-					echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home">' . esc_html( get_bloginfo( 'name' ) ) . '</a>';
-					if ( is_front_page() || is_home() || is_front_page() && is_home() ) {
+					echo '<a href="' . esc_url(home_url('/')) . '" title="' . esc_attr(get_bloginfo('name')) . '" rel="home">' . esc_html(get_bloginfo('name')) . '</a>';
+					if (is_front_page() || is_home() || is_front_page() && is_home()) {
 						echo '</h1>';
 					}
 					?>
 				</div>
-				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
+				<div id="site-description"><?php bloginfo('description'); ?></div>
 			</div>
 			<nav id="menu" role="navigation">
 				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'main-menu',
-						'menu_class'     => 'menu w3-bar w3-section',
-						'walker'         => new W3csspress_Walker_Nav_Menu(),
-					)
-				);
+				if (has_nav_menu('main-menu')) {
+					wp_nav_menu(
+						array(
+							'theme_location' => 'main-menu',
+							'menu_class'     => 'menu w3-bar w3-section',
+							'walker'         => new W3csspress_Walker_Nav_Menu(),
+						)
+					);
+				}
 				?>
 			</nav>
 			<div id="search"><?php get_search_form(); ?></div>
