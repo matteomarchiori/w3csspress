dc50baf0ae65c5ac87e4c82683653fca9a3bb112
Matteo Marchiori <matteo.marchiori97@gmail.com>
2022-04-24 18:37:16 +0200

    Changed text behavior of footer

--- footer.php
+++ footer.php
@@ -15,15 +15,16 @@ namespace w3csspress;
 </div>
 <?php get_sidebar( 'footwidgets' ); ?>
 <footer id="footer" role="contentinfo" class="w3-border-top w3-padding-top w3-center w3-container">
-	<span id="copyright">
-		&copy; <?php echo esc_html( date_i18n( __( 'Y', 'w3csspress' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-	</span>
 	<?php
 	$w3csspress_footer = get_option( 'w3csspress_footer' );
 	if ( '' !== $w3csspress_footer ) {
 		?>
 	<span>
 		<?php echo esc_html( $w3csspress_footer ); ?>
+	</span>
+	<?php } else { ?>
+	<span id="copyright">
+		&copy; <?php echo esc_html( date_i18n( __( 'Y', 'w3csspress' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
 	</span>
 	<?php } ?>
 </footer>
